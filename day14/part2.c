4f68edcb145976669a4ed342fdb0dd95a19dc235
Jeff Hillman <jeff.hillman@gmail.com>
2020-12-14 19:45:41 -0700

    Day 14

--- day14/part2.c
+++ day14/part2.c
@@ -8,37 +8,75 @@ int main() {
 
     if (computer) {
         char mask[MASK_LENGTH + 1];
-        long bit = 1;
+        long bit;
+        long floatingBitCount;
+        long floatingBits;
         char *maskPtr;
+        long memoryOffset;
         long value;
 
         long memorySum = 0;
 
         for (int i = 0; i < computer->instructionCount; i++) {
+            if (i == 0) {
+                printf("here\n");
+                strcpy(mask, "000000000000000000000000000000X1001X");
+            }
             switch (computer->instructions[i].type) {
                 case MASK:
                     strcpy(mask, computer->instructions[i].mask);
                     break;
                 case MEMORY:
                     bit = 1;
+                    floatingBitCount = 0;
+                    floatingBits = 0;
                     maskPtr = mask + MASK_LENGTH - 1;
+                    memoryOffset = computer->instructions[i].memoryOffset;
                     value = computer->instructions[i].value;
 
                     while (maskPtr >= mask) {
                         if (*maskPtr == '1' && (value & bit) == 0) {
-                            value += bit;
-                        } else if (*maskPtr == '0' && (value & bit) == bit) {
-                            value -= bit;
+                            memoryOffset += bit;
+                        } else if (*maskPtr == 'X') {
+                            ++floatingBitCount;
+                            floatingBits += bit;
                         }
 
                         --maskPtr;
                         bit <<= 1;
                     }
 
-                    memorySum -= computer->memory[computer->instructions[i].memoryOffset];
-                    memorySum += value;
+                    long floatingBitOptions = 1LL << floatingBitCount;
 
-                    computer->memory[computer->instructions[i].memoryOffset] = value;
+                    printf("%ld, %ld, %ld\n", floatingBitCount, floatingBits, floatingBitOptions);
+
+                    for (int j = 0; j < floatingBitOptions; j++) {
+                        long long floatingMemoryOffset = memoryOffset;
+                        bit = 1;
+
+                        for (int k = 0; k < floatingBitCount; k++) {
+                            while ((bit & floatingBits) != bit) {
+                                bit <<= 1;
+                            }
+
+                            printf("bit: %ld\n", bit);
+
+                            if ((j & bit) == bit) {
+                                floatingMemoryOffset += bit;
+                            } else if ((j & bit) == j) {
+                                floatingMemoryOffset -= bit;
+                            }
+
+                            bit <<= 1;
+                        }
+
+                        printf("memory offset: %lld\n", floatingMemoryOffset);
+
+                        memorySum -= computer->memory[floatingMemoryOffset];
+                        memorySum += value;
+
+                        computer->memory[floatingMemoryOffset] = value;
+                    }
                     break;
             }
         }
